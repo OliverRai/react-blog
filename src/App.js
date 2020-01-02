42435da5cb1448532d90571bb6ced793915efe37
OliverRai <raiane.mateus.oliveira@gmail.com>
2020-01-01 23:40:44 -0300

    firebase class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import firebase from './firebase';
+
+import Home from './components/Home';
+import Header from './components/Header';
 import './global.css';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+class App extends Component{
+
+state = {
+  firebaseInitialized: false
+};
+
+componentDidMount(){
+  firebase.isInitialized().then(resultado =>{
+    //devolve user
+    this.setState({firebaseInitialized: resultado});
+  })
+}
+
+  render(){
+    return this.state.firebaseInitialized !== false ?(
+      <BrowserRouter>
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+        </Switch>
+      </BrowserRouter>
+    ) : (
+      <h1>Carregando...</h1>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,44 @@
+import app from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+
+let firebaseConfig = {
+    apiKey: "AIzaSyBwftAFPD8XYU4sVXDyAItVYY2vXrlnUJ4",
+    authDomain: "react-app-e5278.firebaseapp.com",
+    databaseURL: "https://react-app-e5278.firebaseio.com",
+    projectId: "react-app-e5278",
+    storageBucket: "react-app-e5278.appspot.com",
+    messagingSenderId: "172658392649",
+    appId: "1:172658392649:web:8654080d79330e7a4ac998",
+    measurementId: "G-HHHKWN9K08"
+  };
+
+class firebase{
+    constructor(){
+        app.initializeApp(firebaseConfig);
+    }
+
+    login(email, password){
+        return app.auth().signInWithEmailAndPassword(email, password); 
+    }
+
+    async register(nome, email, password){
+        await app.auth().createUserWithEmailAndPassword(nome, email, password);
+
+        const uid = app.auth().currentUser.uid;
+
+        return app.database().ref('users').child(uid).set({
+            nome: nome
+        })   
+    }
+
+    isInitialized(){
+        return new Promise(resolve =>{
+            app.auth().onAuthStateChanged(resolve);
+        })
+    }
+}
+
+export default new firebase;
+
+
